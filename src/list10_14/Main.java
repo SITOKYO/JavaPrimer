e50a61fa44ee9d2cd446520fdeb398f385f1e400
kinoshita_h <kinoshita_h@T1994019B>
2015-09-15 14:20:47 +0900

    System.out.println("名前は" + h.getName()) を追加

--- src/list10_14/Main.java
+++ src/list10_14/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
 	Hero h = new Hero();
 	h.setName("");		// 長さ0文字の名前をセットしようとする
+        System.out.println("名前は" + h.getName());
     }
 }
